76897f6da5cf75d030321cc8ad7572f2e5153b53
George Tian <github@georgetian.com>
2023-06-11 12:14:28 +0800

    commit

--- src/backend/controllers/content.py
+++ src/backend/controllers/content.py
@@ -115,6 +115,7 @@ mime_types = MimeTypes()
 async def get_media(media_id: int):
     path = content_service.media_id_to_path(media_id)
     media_type = magic.from_file(path, mime=True)
+    print('media type:', media_type)
     filename = path.with_suffix(mime_types.guess_extension(media_type)).name
     try:
         return await download_file(path, filename=filename, media_type=media_type)
